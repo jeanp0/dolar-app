271383a40a825f5a9dacaf7de13b29357f3daf9a
jeanp0 <jeanpi3rm@gmail.com>
2021-01-06 07:08:42 -0500

    first

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loading: {
+      title: "",
+      state: false,
+      color: "primary",
+    },
   },
   mutations: {
+    showLoader(state, payload) {
+      state.loading.title = payload.title;
+      state.loading.state = true;
+      state.loading.color = payload.color;
+    },
+    hiddenLoader(state) {
+      state.loading.state = false;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="my-5 text-center">Dolar a peso chileno App</h1>
+        <v-card>
+          <v-date-picker
+            header-color="blue darken-1"
+            color="green lighten-1"
+            v-model="date"
+            full-width
+            locale="es-ec"
+            :min="min"
+            :max="max"
+            @change="setDolarValue(dateFormated)"
+          >
+          </v-date-picker>
+        </v-card>
+        <v-card color="green darken-1" dark>
+          <v-card-text class="display-1 text-center"> {{ valor }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from "axios";
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      date: this.getActualDate(),
+      min: "1984",
+      max: this.getActualDate(),
+      valor: null,
+    };
+  },
+  methods: {
+    ...mapMutations(["showLoader", "hiddenLoader"]),
+    async setDolarValue(dateSelected) {
+      try {
+        this.showLoader({
+          title: "Accediendo a la información",
+          color: "secondary",
+        });
+        const datos = await axios(
+          `https://mindicador.cl/api/dolar/${dateSelected}`
+        );
+        this.valor =
+          datos.data.serie.length > 0
+            ? datos.data.serie[0].valor
+            : "Sin resultados";
+      } catch (e) {
+        console.log(e.message);
+      } finally {
+        console.log("API consumida");
+        this.hiddenLoader();
+      }
+    },
+    getActualDate() {
+      return new Date().toISOString().substr(0, 10);
+    },
+  },
+  computed: {
+    dateFormated() {
+      return this.date.split("-").reverse().join("-");
+    },
+  },
+
+  created() {
+    this.setDolarValue(this.dateFormated);
+  },
+};
 </script>
+
